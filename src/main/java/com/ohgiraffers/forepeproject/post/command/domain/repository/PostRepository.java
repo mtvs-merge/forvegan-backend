a3b1cfdf5070e24e3c948e978e2f29665235d91a
ej20001000 <ejayyj1225@gmail.com>
2023-07-20 22:41:37 +0900

    fix: post/main merge

--- src/main/java/com/ohgiraffers/forepeproject/post/command/domain/repository/PostRepository.java
+++ src/main/java/com/ohgiraffers/forepeproject/post/command/domain/repository/PostRepository.java
@@ -1,11 +1,9 @@
 package com.ohgiraffers.forepeproject.post.command.domain.repository;
 
 import com.ohgiraffers.forepeproject.post.command.domain.aggregate.entity.PostEntity;
-import com.ohgiraffers.forepeproject.post.command.domain.aggregate.vo.PostVO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.transaction.Transactional;
 
 @Repository
 public interface PostRepository extends JpaRepository<PostEntity, Integer> {
