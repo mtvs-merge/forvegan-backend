79ff67f4ec540a2a73ff69bd13af01be1e7e1fc1
ej20001000 <ejayyj1225@gmail.com>
2023-07-20 22:41:37 +0900

    feat: 게시글 단건 삭제 기능 구현

--- src/main/java/com/ohgiraffers/forepeproject/post/command/application/controller/PostDeleteController.java
+++ src/main/java/com/ohgiraffers/forepeproject/post/command/application/controller/PostDeleteController.java
@@ -1,4 +1,30 @@
 package com.ohgiraffers.forepeproject.post.command.application.controller;
 
+
+import com.ohgiraffers.forepeproject.post.command.application.service.PostDeleteService;
+import com.ohgiraffers.forepeproject.post.command.domain.aggregate.entity.enumType.ResponesEnum;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/post/delete")
 public class PostDeleteController {
+
+    private final PostDeleteService postDeleteService;
+
+    @Autowired
+    public PostDeleteController(PostDeleteService postDeleteService){
+        this.postDeleteService = postDeleteService;
+    }
+
+    @PostMapping("/row")
+    public ResponesEnum deletePost(@RequestParam Long postNum) {
+        return postDeleteService.deletePost(postNum);
+    }
+
 }
--- src/main/java/com/ohgiraffers/forepeproject/post/command/application/service/PostDeleteService.java
+++ src/main/java/com/ohgiraffers/forepeproject/post/command/application/service/PostDeleteService.java
@@ -1,4 +1,26 @@
 package com.ohgiraffers.forepeproject.post.command.application.service;
 
+import com.ohgiraffers.forepeproject.post.command.domain.aggregate.entity.enumType.ResponesEnum;
+import com.ohgiraffers.forepeproject.post.command.domain.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PostDeleteService {
+
+    private final PostRepository postRepository;
+
+    @Autowired
+    public PostDeleteService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    public ResponesEnum deletePost(Long postNum) {
+        try {
+            postRepository.deleteById(postNum);
+            return ResponesEnum.SUCCESS;
+        } catch (Exception exception) {         // 데이터 없을 시 에러?
+            return ResponesEnum.FAILURE;
+        }
+    }
 }
