750e8cb34a2aba2dbc16885f453792c9d0c33e20
ej20001000 <ejayyj1225@gmail.com>
2023-07-22 22:06:45 +0900

    feat: 게시글 수정 기능 구현

--- src/main/java/com/ohgiraffers/forepeproject/post/command/application/service/PostModifyService.java
+++ src/main/java/com/ohgiraffers/forepeproject/post/command/application/service/PostModifyService.java
@@ -1,4 +1,28 @@
 package com.ohgiraffers.forepeproject.post.command.application.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ohgiraffers.forepeproject.post.command.application.dto.PostDTO;
+import com.ohgiraffers.forepeproject.post.command.application.dto.PostDeleteDTO;
+import com.ohgiraffers.forepeproject.post.command.domain.aggregate.entity.PostEntity;
+import com.ohgiraffers.forepeproject.post.command.domain.repository.PostRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
 public class PostModifyService {
+
+    private final PostRepository postRepository;
+
+    @Autowired
+    public PostModifyService(PostRepository postRepository) { this.postRepository = postRepository; }
+
+    @Transactional
+    public void modifyAll(PostEntity postEntity) {
+
+        postRepository.save(postEntity);
+        postRepository.flush();
+    }
 }
