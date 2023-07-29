7415f14f6b4ea7ded21ea9a9be92f9497a67bf83
ej20001000 <ejayyj1225@gmail.com>
2023-07-30 03:34:12 +0900

    feat: post read list 프론트 연결 완료

---
+++ src/main/java/com/ohgiraffers/forepeproject/post/query/domain/entity/PostMapper.java
@@ -0,0 +1,11 @@
+package com.ohgiraffers.forepeproject.post.query.domain.entity;
+
+import com.ohgiraffers.forepeproject.post.command.domain.aggregate.entity.PostEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface PostMapper {
+    List<PostEntity> getAllPost(int page, int postCategoryNum);
+}
--- src/main/java/com/ohgiraffers/forepeproject/post/query/domain/repository/Repository.java
+++
@@ -1,4 +0,0 @@
-package com.ohgiraffers.forepeproject.post.query.domain.repository;
-
-public class Repository {
-}
