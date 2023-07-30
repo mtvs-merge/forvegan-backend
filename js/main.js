bb718735eb893c7746ed1b5b9c887d2db42a9af8
Soyeon Park <aqua343@gmail.com>
2023-07-31 00:15:04 +0900

    커밋메세지 TEST

---
+++ js/main.js
@@ -0,0 +1,151 @@
+(function ($) {
+    "use strict";
+
+    $(function(){ 
+
+        $("#imageFile").on("change", function(event) {
+
+            var file = event.target.files[0];
+        
+            var reader = new FileReader(); 
+            reader.onload = function(e) {
+        
+                $("#preview").attr("src", e.target.result);
+            }
+        
+            reader.readAsDataURL(file);
+        
+        });
+
+        $("#imageFile2").on("change", function(event) {
+
+            var file = event.target.files[0];
+        
+            var reader = new FileReader(); 
+            reader.onload = function(e) {
+        
+                $("#preview").attr("src", e.target.result);
+            }
+        
+            reader.readAsDataURL(file);
+        
+        });
+
+
+     
+
+        
+
+
+
+        $(".call").click(function(){
+          $(".modal").fadeIn();
+        });
+        
+        $(".modal_report", "exit").click(function(){
+          $(".modal").fadeOut();
+        });
+        
+        });
+
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+
+    //헤더 서치영역 열기
+    $(".btnS").click(function(){
+        $(".search").slideToggle(500);
+        return false;
+    });
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+        } else {
+            $('.sticky-top').removeClass('shadow-sm').css('top', '-150px');
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Modal Video
+    var $videoSrc;
+    $('.btn-play').click(function () {
+        $videoSrc = $(this).data("src");
+    });
+    console.log($videoSrc);
+    $('#videoModal').on('shown.bs.modal', function (e) {
+        $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+    })
+    $('#videoModal').on('hide.bs.modal', function (e) {
+        $("#video").attr('src', $videoSrc);
+    })
+
+
+    // Product carousel
+    $(".product-carousel").owlCarousel({
+        autoplay: false,
+        smartSpeed: 1000,
+        margin: 25,
+        loop: true,
+        center: true,
+        dots: false,
+        nav: true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ],
+        // responsive: {
+		// 	0:{
+        //         items:1
+        //     },
+        //     576:{
+        //         items:1
+        //     },
+        //     768:{
+        //         items:2
+        //     },
+        //     992:{
+        //         items:3
+        //     }
+        // }
+    });
+
+
+    // Testimonial carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        items: 1,
+        loop: true,
+        dots: true,
+        nav: false,
+    });
+    
+})(jQuery);
+
